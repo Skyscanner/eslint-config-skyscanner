c22113ab2a032e4630580d1e1428542dd30871e1
Tim von Oldenburg <tim.vonoldenburg@skyscanner.net>
2017-05-23 09:37:25 +0100

    Add e2e test

---
+++ test/e2e-test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cp .package.json package.json
+
+PEER_DEPENDENCIES=$(node -p "JSON.stringify(require('../package.json').peerDependencies, null, 2)")
+echo "$PEER_DEPENDENCIES" | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev
+
+npm install
+npm test
