b5ee4fb9cb9652cb9ca9409e9361391653b173c1
Dan Ristea <dan.ristea@skyscanner.net>
2017-06-07 19:02:18 +0100

    Add --no-shrinkwrap to prevent tests failing when running with npm 5

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -5,5 +5,5 @@ cp .package.json package.json
 PEER_DEPENDENCIES=$(node -p "JSON.stringify(require('../package.json').peerDependencies, null, 2)")
 echo "$PEER_DEPENDENCIES" | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev
 
-npm install
+npm install --no-shrinkwrap
 npm test
