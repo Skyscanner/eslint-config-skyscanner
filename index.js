c9296e5971de6331852bcda634ceb41f65e71c13
Jaume Salgado <jaume.salgado@skyscanner.net>
2020-04-19 23:19:52 +0200

    Remove "eslint-plugin-jest-dom"

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ module.exports = {
     'plugin:eslint-comments/recommended',
     'plugin:jest/recommended',
     'plugin:jest-formatting/strict',
-    'plugin:jest-dom/recommended',
   ],
   plugins: ['backpack', 'prettier', 'jest-formatting'],
   rules: {
