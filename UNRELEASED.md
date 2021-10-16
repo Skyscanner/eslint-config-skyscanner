e402cb78df47bbec18ebffe9dee2ade7358be64f
Dominic Fraser <dominic.fraser@skyscanner.net>
2021-10-16 16:52:32 +0100

    add typescript support

--- UNRELEASED.md
+++ UNRELEASED.md
@@ -2,6 +2,16 @@
 
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## Added
+- Support TypeScript. Adds overrides for `.ts?(x)` files taken from react-scripts, adds `@typescript-eslint/eslint-plugin` and `@typescript-eslint/parser`
+- Adds config for `object` and `type` imports under `import/order`
+- Adds config for `.ts?(x)` extensions under `import/extensions`
+- Adds `eslint-plugin-typescript-enum` to disallow TypeScript language expression feature `enum`
+
+## Upgraded
+- Bump `eslint-plugin-import` from `2.22.1` to `2.25.2`
+
+
 **Breaking/Added:**
 -  Bump `prettier` from `2.2.1` to `2.3.0`
 -  Bump eslint-plugin-react from 7.23.2 to 7.24.0 
--- index.js
+++ index.js
@@ -20,8 +20,10 @@ module.exports = {
     'plugin:jest/recommended',
     'plugin:jest-formatting/strict',
     'plugin:react-hooks/recommended',
+    'plugin:import/typescript',
+    'plugin:typescript-enum/recommended',
   ],
-  plugins: ['backpack', 'prettier', 'jest-formatting'],
+  plugins: ['backpack', 'prettier', 'jest-formatting', 'typescript-enum'],
   rules: {
     'prettier/prettier': 'error',
     'valid-jsdoc': ['error'],
@@ -97,8 +99,31 @@ module.exports = {
     'import/order': [
       'error',
       {
-        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        groups: [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
         'newlines-between': 'always',
+      },
+    ],
+
+    // Ensure consistent use of file extension within the import path
+    // The airbnb config we extend does not support TypeScript so we override it here
+    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/extensions.md
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        mjs: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
 
@@ -124,4 +149,68 @@ module.exports = {
 
     'jest/no-large-snapshots': 'off',
   },
+  overrides: [
+    // TS linting configuration is taken directly from react-scripts: https://github.com/facebook/create-react-app/blob/9d0369b1fe3260e620b08effcf85f1edefc5d1ea/packages/eslint-config-react-app/index.js#L31
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+      // If adding a typescript-eslint version of an existing ESLint rule,
+      // make sure to disable the ESLint rule here.
+      rules: {
+        // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
+        'default-case': 'off',
+        // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
+        'no-dupe-class-members': 'off',
+        // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
+        'no-undef': 'off',
+
+        // Add TypeScript specific rules (and turn off ESLint equivalents)
+        '@typescript-eslint/consistent-type-assertions': 'warn',
+        'no-array-constructor': 'off',
+        '@typescript-eslint/no-array-constructor': 'warn',
+        'no-redeclare': 'off',
+        '@typescript-eslint/no-redeclare': 'warn',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': [
+          'warn',
+          {
+            functions: false,
+            classes: false,
+            variables: false,
+            typedefs: false,
+          },
+        ],
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': [
+          'error',
+          {
+            allowShortCircuit: true,
+            allowTernary: true,
+            allowTaggedTemplates: true,
+          },
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            args: 'none',
+            ignoreRestSiblings: true,
+          },
+        ],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'warn',
+      },
+    },
+  ],
 };
