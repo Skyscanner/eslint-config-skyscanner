e773747b84e6517b6352e0632470bd9f2794a454
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2022-11-28 09:46:00 +0000

    Disable default import of react

--- index.js
+++ index.js
@@ -220,6 +220,24 @@ module.exports = {
           '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
       },
     ],
+
+    // This ensures that the 'preferred style' is used for react imports:
+    // "
+    //   Change all default React imports (i.e. import React from "react") to destructured
+    //   named imports (ex. import { useState } from "react") which is the preferred style
+    //   going into the future.
+    // "
+    // There is a codemod here created by the react team to change this in your codebase:
+    // https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#removing-unused-react-imports
+    'no-restricted-imports': [
+      'error',
+      {
+        name: 'react',
+        importNames: ['default'],
+        message:
+          "Please import directly (e.g. import { useEffect } from 'react').",
+      },
+    ],
   },
   overrides: [
     {
