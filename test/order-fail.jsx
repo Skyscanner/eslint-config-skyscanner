b41bbef303d3e9c835e2d06c7f4c0d105775e710
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2023-10-17 15:31:56 +0100

    [ARGG-741]: Add rule to set alphabetical order of imports (#623)

---
+++ test/order-fail.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars,import/no-unresolved,import/extensions */
+// import/order
+import ReactDom from 'react-dom';
+import PropTypes from 'prop-types';
+import { Component, Fragment } from 'react';
+
+import { fontSizeSm } from '@skyscanner/bpk-foundations-web/tokens/base.es6';
+import ArrowUpIcon from '@skyscanner/backpack-web/bpk-component-icon/sm/long-arrow-up';
+import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
+import BpkButtonLink from '@skyscanner/backpack-web/bpk-component-link';
+
+import Component1 from '../Component1';
+import Component2 from '../../Component2';
+/* eslint-enable no-unused-vars,import/no-unresolved,import/extensions */
