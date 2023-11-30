b1113262c5c9c4f4447460f8cacac9400b9f8b97
Patryk Szulczyk <patryk.szulczyk@skyscanner.net>
2023-11-30 16:21:00 +0100

    Simpler metadata

--- eslint-plugin-skyscanner-no-axios/src/index.js
+++ eslint-plugin-skyscanner-no-axios/src/index.js
@@ -1,14 +1,6 @@
 const noAxios = require('./rules/no-axios');
 
 module.exports = {
-  configs: {
-    error: {
-      plugins: ['skyscanner-no-axios'],
-      rules: {
-        'skyscanner-no-axios/no-axios': 'error',
-      },
-    },
-  },
   rules: {
     'no-axios': noAxios,
   },
