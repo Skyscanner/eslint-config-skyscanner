06a7621a1c81d69ce89d12d2eecc77c972241186
Patryk Szulczyk <patryk.szulczyk@skyscanner.net>
2023-11-30 16:45:13 +0100

    Change comment

--- test/no-axios-fail.jsx
+++ test/no-axios-fail.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars,import/no-unresolved,import/extensions */
-// import/order
+// skyscanner-no-axios/no-axios
 import axios from 'axios';
 
 /* eslint-enable no-unused-vars,import/no-unresolved,import/extensions */
