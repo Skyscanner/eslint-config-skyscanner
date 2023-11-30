12cb6c6ad40b3cd3bf306b26398264afdc058023
Patryk Szulczyk <patryk.szulczyk@skyscanner.net>
2023-11-30 16:43:38 +0100

    Add no-axios e2e test

---
+++ test/no-axios-fail.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars,import/no-unresolved,import/extensions */
+// import/order
+import axios from 'axios';
+
+/* eslint-enable no-unused-vars,import/no-unresolved,import/extensions */
